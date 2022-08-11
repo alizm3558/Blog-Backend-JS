828e941ea8e3734ce92806e45bdb0d63ceb68308
alizm3558 <alibakir3558@gmail.com>
2022-08-11 15:30:14 +0300

    -

--- routers/auth.js
+++ routers/auth.js
@@ -1,16 +1,16 @@
-const express=require('express');
+const express = require('express');
 
-const router=express.Router();
-const User=require("../models/User");
-const Joi=require("@hapi/joi");
+const router = express.Router();
+const User = require("../models/User");
+const Joi = require("@hapi/joi");
 
 
-router.post("/register",(req,res)=>{
+router.post("/register", (req, res) => {
 
     res.send(req.body.email);
-    
- 
- });
 
 
- module.exports=router;
+});
+
+
+module.exports = router;
